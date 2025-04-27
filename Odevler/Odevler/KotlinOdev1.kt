f7a5c25aa2c90758e6b568fb1fb23ad6fa6a970f
Esra Eda Kılıç <edakilic440@gmail.com>
2025-04-27 12:48:06 +0300

    Odev 1 Eklendi

---
+++ Odevler/Odevler/KotlinOdev1.kt
@@ -0,0 +1,150 @@
+package com.example.kotlinodev1
+
+fun main() {
+    // 1. Grup: Kisisel Bilgiler
+
+    val sehir = "Ankara"
+    println("Şehir: $sehir")
+
+    val ulke = "Türkiye"
+    println("Ülke: $ulke")
+
+    var telefon = "0530 444 4444"
+    println("Telefon: $telefon")
+
+    val postaKodu = 61604
+    println("Posta Kodu: $postaKodu")
+
+    val email = "edakilic440@gmail.com"
+    println("Email: $email")
+
+    var meslek = "Öğrenci"
+    println("Meslek: $meslek")
+
+    var stokMiktari = 2000
+    println("Stok Miktarı: $stokMiktari")
+
+    var musteriAdi = "Aslı"
+    println("Müşteri Adı: $musteriAdi")
+
+    var bakiye = 3400.45
+    println("Bakiye: $bakiye")
+
+    val dogumGunu = "08.01.2003"
+    println("Doğum Günü: $dogumGunu")
+
+    var maas = 80000.85
+    println("Maaş: $maas")
+
+    var medeniDurum = false
+    println("Medeni Durum: $medeniDurum")
+
+    var urunYorum = "Good product"
+    println("Ürün Yorumu: $urunYorum")
+
+    // 2. Grup: Odeme, Siparis ve Arac Bilgileri
+
+    val odemeTarihi = "01.10.2024"
+    println("Ödeme Tarihi: $odemeTarihi")
+
+    var odeme = "Kredi Kartı"
+    println("Ödeme Şekli: $odeme")
+
+    var siparisAdedi = 200
+    println("Sipariş Adedi: $siparisAdedi")
+
+    val arabaModeli = "Peugeot 308"
+    println("Araba Modeli: $arabaModeli")
+
+    val kitapAdi = "Momo"
+    println("Kitap Adı: $kitapAdi")
+
+    val yayinlamaTarihi = "08.08.2000"
+    println("Yayınlama Tarihi: $yayinlamaTarihi")
+
+    var indirimMiktari = 30
+    println("İndirim Miktarı: $indirimMiktari%")
+
+    var odaSayisi = 8
+    println("Oda Sayısı: $odaSayisi")
+
+    var enlem = 24.5
+    println("Enlem: $enlem")
+
+    var boylam = 32.0
+    println("Boylam: $boylam")
+
+    val urunAdi = "Kitap"
+    println("Ürün Adı: $urunAdi")
+
+    var yemekFiyati = 350
+    println("Yemek Fiyatı: $yemekFiyati")
+
+    // 3. Grup: Ürün, Marka ve Medya Bilgileri
+
+    val marka = "Pupilica"
+    println("Marka: $marka")
+
+    val muzikAdi = "Ocean Drive"
+    println("Müzik Adı: $muzikAdi")
+
+    var videoSuresi = 4.2
+    println("Video Süresi: $videoSuresi dakika")
+
+    var urunPuani = 4.7
+    println("Ürün Puanı: $urunPuani")
+
+    val resimAdi = "Son Akşam Yemeği"
+    println("Resim Adı: $resimAdi")
+
+    var dosyaFormati = "PDF"
+    println("Dosya Formatı: $dosyaFormati")
+
+    val renk = "Mavi"
+    println("Renk: $renk")
+
+    var renkKodu = "#0000ff"
+    println("Renk Kodu: $renkKodu")
+
+    val telefonModeli = "Samsung S23"
+    println("Telefon Modeli: $telefonModeli")
+
+    var ekranBoyutu = 6.1
+    println("Ekran Boyutu: $ekranBoyutu inç")
+
+    val agirlik = 168
+    println("Ağırlık: $agirlik kg")
+
+    // 4. Grup: Rezervasyon, Tarih
+
+    val ulusalGun = "19 Mayıs"
+    println("Ulusal Gün: $ulusalGun")
+
+    val tatilGunu = "1 Mayıs"
+    println("Tatil Günü: $tatilGunu")
+
+    var rezervasyonTarihi = "18.06.2025"
+    println("Rezervasyon Tarihi: $rezervasyonTarihi")
+
+    val sokakAdi = "Akşemsettin"
+    println("Sokak Adı: $sokakAdi")
+
+    var otobusHatti = "340"
+    println("Otobüs Hattı: $otobusHatti")
+
+    var kalanDakika = 6
+    println("Kalan Dakika: $kalanDakika")
+
+    val takipKodu = 356478545
+    println("Takip Kodu: $takipKodu")
+
+    var kuponSuresi = "14 dakika"
+    println("Kupon Süresi: $kuponSuresi")
+
+    val kuponKodu = 56852
+    println("Kupon Kodu: $kuponKodu")
+
+    var faturaAdresi = "Ev"
+    println("Fatura Adresi: $faturaAdresi")
+
+}
